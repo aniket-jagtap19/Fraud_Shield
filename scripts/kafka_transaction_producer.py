47c88e6462ce637cc0541e6f551ab2ed5ccb7a31
Aniket Jagtap <jagtapaniket731712@gmail.com>
2026-08-28 22:58:40 +0530

    some fixes

--- scripts/kafka_transaction_producer.py
+++ scripts/kafka_transaction_producer.py
@@ -53,3 +53,5 @@ if __name__ == "__main__":
 # Automated maintenance update - 2026-08-28 22:40:04
 
 # Project by Aniket Jagtap, time: - 2026-08-28 22:48:24
+
+# Project by Aniket Jagtap, time: - 2026-08-28 22:58:43
